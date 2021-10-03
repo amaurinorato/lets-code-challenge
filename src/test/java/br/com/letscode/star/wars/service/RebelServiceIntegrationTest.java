6635844e123675bcd6cd1e3a6f2477a4b7be0103
amaurinorato <amauri.norato@gmail.com>
2021-10-03 16:59:26 -0300

    removed unused imports

--- src/test/java/br/com/letscode/star/wars/service/RebelServiceIntegrationTest.java
+++ src/test/java/br/com/letscode/star/wars/service/RebelServiceIntegrationTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.lang.reflect.Executable;
 import java.util.Collections;
 
 @RunWith(SpringJUnit4ClassRunner.class)
