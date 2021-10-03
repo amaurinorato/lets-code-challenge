0be594c2559cd62372d0b2c5993910a4347b4b89
amaurinorato <amauri.norato@gmail.com>
2021-10-03 16:58:29 -0300

    added swagger doc

--- src/main/java/br/com/letscode/star/wars/controller/DealController.java
+++ src/main/java/br/com/letscode/star/wars/controller/DealController.java
@@ -5,9 +5,14 @@ import br.com.letscode.star.wars.dto.request.DealRequestDto;
 import br.com.letscode.star.wars.dto.request.RebelRequestDto;
 import br.com.letscode.star.wars.service.DealService;
 import br.com.letscode.star.wars.service.RebelService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,9 +24,13 @@ public class DealController {
     @Autowired
     DealService dealService;
 
+    @ApiOperation(value = "Operation to make deals between two rebels", nickname = "MakeDeal")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "When the service can exchange items between rebels", response = Rebel.class, responseContainer = "Object"),
+            @ApiResponse(code = 400, message = "When there is some invalid data in the request ", response = ResponseStatusException.class)})
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping
     public ResponseEntity<List<Rebel>> postDeal(@RequestBody DealRequestDto dealDto) {
-        System.out.println("dealDto: " + dealDto);
         return ResponseEntity.ok(dealService.makeDeal(dealDto));
     }
 
--- src/main/java/br/com/letscode/star/wars/controller/RebelsController.java
+++ src/main/java/br/com/letscode/star/wars/controller/RebelsController.java
@@ -4,9 +4,14 @@ import br.com.letscode.star.wars.domain.Rebel;
 import br.com.letscode.star.wars.dto.request.LocationRequestDto;
 import br.com.letscode.star.wars.dto.request.RebelRequestDto;
 import br.com.letscode.star.wars.service.RebelService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @CrossOrigin
 @RestController
@@ -16,23 +21,33 @@ public class RebelsController {
     @Autowired
     RebelService rebelService;
 
+    @ApiOperation(value = "Operation to save a rebel", nickname = "SaveRebel")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "When the service can save the informed rebel ", response = Rebel.class, responseContainer = "Object"),
+            @ApiResponse(code = 400, message = "When there is some invalid data in the request ", response = ResponseStatusException.class)})
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<Rebel> saveRebel(@RequestBody RebelRequestDto rebelDto) {
-        System.out.println("rebelDto: " + rebelDto);
-        System.out.println(rebelDto.getInventory().get(0).getItemType().getPoint());
-        return ResponseEntity.ok(rebelService.save(rebelDto));
+        return new ResponseEntity<>(rebelService.save(rebelDto), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Operation to update rebel location", nickname = "UpdateLocation")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "When the service can update location", response = Rebel.class, responseContainer = "Object"),
+            @ApiResponse(code = 400, message = "When there is some invalid data in the request ", response = ResponseStatusException.class)})
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{rebelId}/locations")
     public ResponseEntity<Rebel> updateRebelLocation(@RequestBody LocationRequestDto locationDto, @PathVariable(value = "rebelId") Long rebelId) {
-        System.out.println("LocationDto: " + locationDto);
-        System.out.println("rebelId: " + rebelId);
         return ResponseEntity.ok(rebelService.updateLocation(locationDto, rebelId));
     }
 
+    @ApiOperation(value = "Operation to inform a traitor rebel", nickname = "UpdateTraitor")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "When the service can update traitor information", response = Rebel.class, responseContainer = "Object"),
+            @ApiResponse(code = 400, message = "When there is some invalid data in the request", response = ResponseStatusException.class)})
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{rebelId}/betrayals")
     public ResponseEntity<Rebel> informTraitor(@PathVariable(value = "rebelId") Long rebelId) {
-        System.out.println("rebelId: " + rebelId);
         return ResponseEntity.ok(rebelService.informTraitor(rebelId));
     }
 }
